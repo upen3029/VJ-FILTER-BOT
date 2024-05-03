2c7c8669743d47f1c9d1ec842a38f6b17384ec6e
upen3029 <125256422+upen3029@users.noreply.github.com>
2024-05-03 22:04:28 +0530

    Update info.py

--- info.py
+++ info.py
@@ -50,7 +50,7 @@ FILE_STORE_CHANNEL = [int(ch) for ch in (environ.get('FILE_STORE_CHANNEL', '')).
 DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in environ.get('DELETE_CHANNELS', '0').split()]
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://ytpremium422024:uWSQVhe3NSTRVWt5@anno3029.5aegjww.mongodb.net/?retryWrites=true&w=majority&appName=anno3029")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://upen3029kumbhakar:FTWNsLHc1fl97BfU@cluster0.cakzsvx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DATABASE_NAME = environ.get('DATABASE_NAME', "anno3029")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'techvjcollection')
 
