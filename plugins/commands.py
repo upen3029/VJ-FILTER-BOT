ee4597a1c582e45a70876042539abd2d4426dcc5
upen3029 <125256422+upen3029@users.noreply.github.com>
2024-05-04 08:03:34 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -621,7 +621,7 @@ async def start(client, message):
             InlineKeyboardButton('Sᴜᴘᴘᴏʀᴛ Gʀᴏᴜᴘ', url=f'https://t.me/{SUPPORT_CHAT}'),
             InlineKeyboardButton('Uᴘᴅᴀᴛᴇs Cʜᴀɴɴᴇʟ', url=CHNL_LNK)
         ],[
-            InlineKeyboardButton("𝗕𝗢𝗧 𝗢𝗪𝗡𝗘𝗥", url="t.me/KingVJ01")
+            InlineKeyboardButton("𝗕𝗢𝗧 𝗢𝗪𝗡𝗘𝗥", url="t.me/anno3029")
         ]]
     msg = await client.send_cached_media(
         chat_id=message.from_user.id,
